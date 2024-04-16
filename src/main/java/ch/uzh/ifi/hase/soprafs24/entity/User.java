a1ff7cff5815c2e547a85b85249894963baa71a0
RobinStirnimann <robin.stirnimann@uzh.ch>
2024-04-16 11:34:24 +0200

    backend guest user fixes #103, fixes #104, fixes #105 fixes #106

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
@@ -40,7 +40,7 @@ public class User implements Serializable {
   @Column(nullable = false)
   private UserStatus status;
 
-  @Column(nullable = false)
+  @Column
   private Boolean isUser;
 
   @Column
