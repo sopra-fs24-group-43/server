93a751d8a44acb88e4812af40cae2c4119e125c5
dominiqueheller <dominiqueheller@yahoo.com>
2024-04-17 14:49:14 +0200

    Added DTO representation to send countdown timer information to client

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websockets/dto/RoundCountdown.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs24.websockets.dto;
+
+public class RoundCountdown {
+    private int time;
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
