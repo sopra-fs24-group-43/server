87f7cddfe82293ca39427da0335c808ca05eaa4f
dominiqueheller <dominiqueheller@yahoo.com>
2024-04-24 12:26:39 +0200

    Added a method in utils for calculating how many points a player should recieve when guessing the word

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/utils/PointCalculator.java
@@ -0,0 +1,17 @@
+package ch.uzh.ifi.hase.soprafs24.utils;
+
+public class PointCalculator {
+
+    public static int calculate(int duration, int answerTime, int answerPosition){
+        double maxPointsTime = 500;
+        double maxPointsPos = 500;
+
+        double PointsTime = (answerTime/duration) * maxPointsTime;
+        double PointsPos = (1/(Math.pow((answerPosition - 0), 1.5))) * maxPointsPos;
+
+        double result = PointsTime + PointsPos;
+
+        return  (int) result;
+    }
+    
+}
