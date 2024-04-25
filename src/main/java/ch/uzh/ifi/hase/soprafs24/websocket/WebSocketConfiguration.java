6e6256f6a553ca70dc972f26275b17e20f8e804a
Florian Mattmüller <florian.mattmueller@uzh.ch>
2024-04-25 15:31:01 +0200

    wsconfig

--- src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketConfiguration.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketConfiguration.java
@@ -1,6 +1,12 @@
 package ch.uzh.ifi.hase.soprafs24.websocket;
 
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import ch.uzh.ifi.hase.soprafs24.repository.GameRepository;
+import ch.uzh.ifi.hase.soprafs24.websocket.dto.inbound.Settings;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,7 +18,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/settings");
+        registry.enableSimpleBroker("/game", "/lobby");
         registry.setApplicationDestinationPrefixes("/app");
     }
 
