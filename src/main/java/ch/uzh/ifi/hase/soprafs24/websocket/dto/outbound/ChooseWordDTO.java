e31beece153a2684b43a47646216af620d61d175
Florian Mattmüller <florian.mattmueller@uzh.ch>
2024-05-15 15:31:49 +0200

    faf2

--- src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/dto/outbound/ChooseWordDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/dto/outbound/ChooseWordDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ChooseWordDTO {
-    private String type; //chooseword or startdrawing
+    private String type; //chooseword when sending to ws (request) or startdrawing when sending to client (response)
     private int wordIndex; //0 to 2
     private String word; //the chosen word
 }
